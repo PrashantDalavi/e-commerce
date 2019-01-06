aad8e004b99ed902aa62ca040038620c88652608
PrashantDalavi <p.d.dalavi@gmail.com>
2019-01-07 00:28:34 +0100

    5.2 form for search bar with some css & bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  get 'simple_pages/landing_page'
   # get '/products', to: 'product#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'simple_pages#landing_page'
-  root 'products#index'
+  root 'simple_pages#landing_page'
   
 
 end
