26acd88c740fa9d916f7c90c3f28c83ee9272d4b
PrashantDalavi <p.d.dalavi@gmail.com>
2019-01-06 10:15:59 +0100

    5.1 routing and redirecting

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  # get '/products', to: 'product#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#landing_page'
+  # root 'simple_pages#landing_page'
+  root 'products#index'
   
 
 end
