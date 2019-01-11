93258630c383538649619bc61b12dcf0fb61ac9a
PrashantDalavi <p.d.dalavi@gmail.com>
2019-01-12 00:57:26 +0100

    5.10 admin role to comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,5 +3,14 @@ class Ability
     def initialize(user)
       user ||= User.new # guest user (not logged in)
       can :manage, User, id: user.id
+
+      if user.present?  # additional permissions for logged in users (they can manage their posts)
+        can :manage, Comment, user_id: user.id 
+        if user.admin?  # additional permissions for administrators
+          can :manage, :all
+        else
+          can :read, :all
+        end
+      end
     end 
 end
