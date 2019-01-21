8a144337ed07cc3f2ca5c0cc25bc60d0638d09b0
PrashantDalavi <p.d.dalavi@gmail.com>
2019-01-21 10:06:33 +0100

    5.3 email setup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-    default from: "from@example.com"
+    default from: "admin@bikershop.com"
 
     def contact_form(email, name, message)
     @message = message
       mail(from: email,
-           to: 'your-email@example.com',
+           to: 'p.d.dalavi@gmail.com',
            subject: "A new contact form message from #{name}")
     end
 end
