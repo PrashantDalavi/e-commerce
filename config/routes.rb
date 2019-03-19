cc1763872a599d3284b3404c0eadbda34610f121
PrashantDalavi <p.d.dalavi@gmail.com>
2019-03-20 00:11:33 +0100

    6.6 creating payments

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'simple_pages#landing_page'
   root 'simple_pages#landing_page'
+
+  post 'payments/create'
 end
